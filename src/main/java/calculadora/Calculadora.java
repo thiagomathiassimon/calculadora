c24049bcef8b00dc44785d66c30a106e1b0295ae
Thiago Mathias Simon <thiago_m_simon@estudante.sc.senai.br>
2022-05-24 20:00:34 -0300

    feat/calculadora

---
+++ src/main/java/calculadora/Calculadora.java
@@ -0,0 +1,49 @@
+package calculadora;
+
+public class Calculadora {
+
+    private double valorA;
+    private double valorB;
+
+    public Calculadora() {
+        this(0.0, 0.0);
+    }
+
+    public Calculadora(double valorA, double valorB) {
+        this.valorA = valorA;
+        this.valorB = valorB;
+    }
+
+    public double getValorA() {
+        return valorA;
+    }
+
+    public void setValorA(double valorA) {
+        this.valorA = valorA;
+    }
+
+    public double getValorB() {
+        return valorB;
+    }
+
+    public void setValorB(double valorB) {
+        this.valorB = valorB;
+    }
+
+    public double getAdicao() {
+        return (getValorA() + getValorB());
+    }
+
+    public double getSubtracao() {
+        return (getValorA() - getValorB());
+    }
+
+    public double getProduto() {
+        return (getValorA() * getValorB());
+    }
+
+    public double getDivisao() {
+        return (getValorA() / getValorB());
+    }
+
+}
