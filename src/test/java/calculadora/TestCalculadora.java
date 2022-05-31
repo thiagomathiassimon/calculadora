efa1d282be852b3f4045cd0f01f13e0e0441c6aa
Thiago Mathias Simon <thiago_m_simon@estudante.sc.senai.br>
2022-05-31 20:50:08 -0300

    ajuste nos testes

--- src/test/java/calculadora/TestCalculadora.java
+++ src/test/java/calculadora/TestCalculadora.java
@@ -19,21 +19,24 @@ public class TestCalculadora {
     @Test
     public void testGetSubtracao() {
         Calculadora calculadora = new Calculadora(4.0, 2.0);
-        double retornoExperado = 2.0;
+        double retornoEsperado = 2.0;
         double retornoFeito = calculadora.getSubtracao();
+        assertEquals(retornoEsperado, retornoFeito, 0);
     }
 
     @Test
     public void testGetProduto() {
         Calculadora calculadora = new Calculadora(4.0, 2.0);
-        double retornoExperado = 8.0;
+        double retornoEsperado = 8.0;
         double retornoFeito = calculadora.getProduto();
+        assertEquals(retornoEsperado, retornoFeito, 0);
     }
 
     @Test
     public void testGetDivisao() {
         Calculadora calculadora = new Calculadora(4.0, 2.0);
-        double retornoExperado = 2.0;
+        double retornoEsperado = 2.0;
         double retornoFeito = calculadora.getDivisao();
+        assertEquals(retornoEsperado, retornoFeito, 0);
     }
 }
